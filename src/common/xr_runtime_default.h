6293b4af0a5123c4c5c9f09088c927dfb42ab800
Nick Klingensmith <programmerpichu@gmail.com>
2021-06-24 21:31:59 -0700

    -Added a runtime from the main branch, couldn't figure a way to merge it nicely D:

--- src/common/xr_runtime_default.h
+++ src/common/xr_runtime_default.h
@@ -10,5 +10,6 @@ windows WMR C:\WINDOWS\system32\MixedRealityRuntime.json
 windows Oculus C:\Program Files\Oculus\Support\oculus-runtime\oculus_openxr_64.json
 windows SteamVR C:\Program Files (x86)\Steam\steamapps\common\SteamVR\steamxr_win64.json
 windows Varjo C:\Program Files\Varjo\varjo-openxr\VarjoOpenXR.json
+windows ViveOpenXR C:\Program Files (x86)\VIVE\Updater\App\ViveVRRuntime\ViveVR_openxr\ViveOpenXR.json
 linux Monado /usr/share/openxr/1/openxr_monado.json
 linux SteamVR ~/.steam/steam/steamapps/common/SteamVR/steamxr_linux64.json)_";
--- src/common/xr_runtime_template.h
+++ src/common/xr_runtime_template.h
@@ -3,8 +3,11 @@ const char *runtime_default_template = R"_(
 # valid one will be used. Items in this file are listed
 # before the default items stored in the code. '~' will 
 # be replaced with the HOME path on Linux.
+#
 # You can find the default configuration list on Github
 # over here: https://github.com/maluoi/openxr-explorer/blob/main/src/common/xr_runtime_default.h
+# Please feel free to make a PR, or raise an issue if you
+# think a path should be added as a default!
 #
 # Format is:
 # [OS id: windows|linux] [name, no space] [path to runtime manifest, spaces are ok]
